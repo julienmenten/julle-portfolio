91316c1623bca050d228f131839b68acaabfe98d
Julle PC <menten.julien@outlook.be>
2021-06-29 00:50:15 +0200

    Created layout and landing + structuring styles

--- julle-portfolio/pages/index.tsx
+++ julle-portfolio/pages/index.tsx
@@ -1,14 +1,16 @@
 import Head from 'next/head'
-import Layout from '../components/Layout/Layout'
-import ParticlesBackground from '../components/Particles/ParticlesBackground'
+import Layout from '../components/Layout/Layout';
+import Landing from '../components/Landing/Landing';
+
 export default function Home() {
   return (
 
     <Layout>
       <Head>
-        <title>Julle - Web developer</title>
+        <title>Julle // Web developer</title>
       </Head>
-      <ParticlesBackground></ParticlesBackground>
+      
+      <Landing />
     </Layout>
   )
 }
